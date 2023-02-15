717b6d669bc8fadaf4215de4690012f390782b5d
Welbert Luiz <welbert.luiz.silva@gmail.com>
2023-02-15 18:25:43 -0300

    Inserido linhas de teste do JAVA
    Inserido linhas de teste do JAVA no VS Code

--- primeiro_projeto/src/primeiro_projeto/OlaMundo.java
+++ primeiro_projeto/src/primeiro_projeto/OlaMundo.java
@@ -4,6 +4,8 @@ public class OlaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("Olá Mundo!");
+		System.out.println("Verificação de codigo em JAVA");
+		System.out.println("Implementação de uma nova linha de teste");
 
 	}
 
